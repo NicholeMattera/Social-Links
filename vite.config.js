8f8d3121089c9e9c1d942f4759cd60037567785e
Nichole Mattera <me@nicholemattera.gay>
2023-01-08 10:36:43 -0600

    ⚡️ Improved perfomance by using WOFF2 and bundling FontAwesome

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
     build: {
+        copyPublicDir: true,
         emptyOutDir: true,
         outDir: '../dist'
     },
     plugins: [
         ViteMinifyPlugin({})
     ],
+    publicDir: '../public',
     root: 'src'
 })
